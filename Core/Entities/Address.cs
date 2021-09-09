e9ae2a88c884b0ccb8ba48726f3b917fe83d4f46
Pal <Pal@DESKTOP-213D8IG>
2021-09-09 23:55:36 +0300

    Add project files.

---
+++ Core/Entities/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Entities
+{
+    public class Address : EntityBase
+    {
+        public string street { get; set; }
+        public string City { get; set; }
+        public int Number { get; set; }
+
+    }
+}
